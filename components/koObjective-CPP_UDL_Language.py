fe2bdaa25827056bd12a6a2758e7759161d6f0a8
Arthur O'Dwyer <arthur@push.am>
2012-11-06 12:02:36 -0800

    Initial commit.
    Released to the public domain.

---
+++ components/koObjective-CPP_UDL_Language.py
@@ -0,0 +1,77 @@
+# Registers the Objective-C++ language in Komodo.
+
+import logging
+from koUDLLanguageBase import KoUDLLanguage
+
+log = logging.getLogger("koObjective-CPPLanguage")
+#log.setLevel(logging.DEBUG)
+
+def registerLanguage(registry):
+    log.debug("Registering language Objective-C++")
+    registry.registerLanguage(KoObjective_CPPLanguage())
+
+class KoObjective_CPPLanguage(KoUDLLanguage):
+
+    # ------------ Komodo Registration Information ------------ #
+
+    name = "Objective-C++"
+    lexresLangName = "Objective_CPP"
+    _reg_desc_ = "%s Language" % name
+    _reg_contractid_ = "@activestate.com/koLanguage?language=%s;1" % name
+    _reg_categories_ = [("komodo-language", name)]
+    _reg_clsid_ = "0a129184-7a16-40e9-866a-3f99bd04646a"
+    defaultExtension = '.mm'
+
+    # ------------ Commenting Controls ------------ #
+
+    commentDelimiterInfo = {
+        "line": [
+                '//',   # C-style one line comments
+                #'#',    # Hash-style one line comments
+                #'--',   # SQL-style one line comments
+                #';',    # Lisp-style one line comments
+                #'%',    # Erlang-style one line comments
+                ],
+        "block": [
+                ('/*', '*/')   # C-style block comments
+                #('(*', '*)')   # Pascal-style block comments
+                ],
+    }
+
+    # ------------ Indentation Controls ------------ #
+
+    # To support automatic indenting and dedenting after "{([" and "})]"
+    supportsSmartIndent = "brace"
+    # Other smart indenting types are:
+    #   'text', 'python', 'XML' and 'keyword'
+
+    # Indent/dedent after these words.
+    _indenting_statements = ['case', 'default', 'protected', 'private', 'public']
+    _dedenting_statements = ['throw', 'return', 'break', 'continue']
+
+    # ------------ Sub-language Controls ------------ #
+
+    #Check: Update 'lang_from_udl_family' as appropriate for your
+    #      lexer definition. There are four UDL language families:
+    #           M (markup), i.e. HTML or XML
+    #           CSL (client-side language), e.g. JavaScript
+    #           SSL (server-side language), e.g. Perl, PHP, Python
+    #           TPL (template language), e.g. RHTML, Django, Smarty
+    #      'lang_from_udl_family' maps each UDL family code (M,
+    #      CSL, ...) to the sub-language name in your language.
+    #      Some examples:
+    #        lang_from_udl_family = {   # A PHP file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'PHP',           #   PHP
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #        }
+    #        lang_from_udl_family = {   # An RHTML file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'Ruby',          #   Ruby
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #           'TPL': 'RHTML',         #   RHTML template code
+    #        }
+    #        lang_from_udl_family = {   # A plain XML can just contain
+    #           'M': 'XML',             #   XML
+    #        }
+    lang_from_udl_family = {'SSL': 'Objective-C++'}
